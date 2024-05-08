381143f3c3c565928beef0e889a57aa61e7e4372
ashishmaurya1109 <ashishmaurya1865@gmail.com>
2024-05-08 21:25:36 +0530

    done till React Elements now start React Functional components

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxElement = <h1 class="heading">Namaste React from jsx👨‍🎓</h1>
-console.log(jsxElement)
+const jsxElement = <h1 id="heading" className="heading">Hello World from jsx!</h1>
+
+//React Components
+// 1- Class based components
+// 2- Functional components
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(jsxElement);
