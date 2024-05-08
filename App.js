db54d81601f14866cf328f0720d6b9db59782c7c
ashishmaurya1109 <ashishmaurya1865@gmail.com>
2024-05-08 19:18:55 +0530

    using jsx instead of React.createElement() for more readability

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {id: "heading1"}, "I am Ashish 👨‍💻");
-const heading2 = React.createElement("h2", {id: "heading2"}, "I am a Developer");
-
-const children1 = React.createElement("div", {id: "child1"}, [heading1 ,heading2])
-const children2 = React.createElement("div", {id: "child2"}, [heading1 ,heading2])
-const parent = React.createElement("div", {id: "parent"}, children1, children2)
-
+const jsxElement = <h1 class="heading">Namaste React from jsx👨‍🎓</h1>
+console.log(jsxElement)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(jsxElement);
