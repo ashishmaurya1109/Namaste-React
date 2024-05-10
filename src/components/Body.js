6c9e82e4d28c1da9024df6ef2dccede849309cc3
ashishmaurya1109 <ashishmaurya1865@gmail.com>
2024-05-10 16:16:24 +0530

    Basic idea of Reconciliation, Diff algo, virtual DOM

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,25 @@
 import { RestaurantCard } from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [topRes, setTopRes] = useState(resList);
+  console.log("topRes", topRes);
+
+  const filterTopRes = () => {
+    const filteredData = resList.filter((elem) => elem.info.avgRating > 4.4);
+    console.log("filteredData", filteredData);
+    setTopRes(filteredData);
+  };
+
   return (
     <div className="body">
       <div className="search">Search</div>
+      <button className="topRated" onClick={filterTopRes}>
+        Top Rated
+      </button>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {topRes.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
